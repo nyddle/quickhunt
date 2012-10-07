c7c324dae98fbd2c83cce3811606ca8829f2e3ad
Yaroslav Glukhov <yarik.glukhov@gmail.com>
2012-10-07 23:39:42 +0400

    Добавил первоначальные шаблоны

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello World!!!'
+    return render_template('registration.html')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
