86adcbaa9e966297c11570d1f133ff5091ef1054
Alexander Davydov <nyddle@gmail.com>
2012-10-08 08:22:47 -0400

    some moudules added

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import os
 from flask import Flask, render_template
 from flask import Flask, request, session, g, redirect, url_for, abort, \
              render_template, flash
-from flask.ext.mail import Mail
-import mailing
+from flask.ext.mail import Mail, Message
 from werkzeug import check_password_hash, generate_password_hash
 
 SECRET_KEY = 'development key'
@@ -17,6 +16,20 @@ mail = Mail(app)
 @app.route('/')
 def hello():
     return render_template('registration.html')
+
+
+def send_awaiting_confirm_mail(user):
+    """
+    Send the awaiting for confirmation mail to the user.
+    """
+    subject = "We're waiting for your confirmation!!"
+    mail_to_be_sent = Message(subject=subject, recipients=[user['email']])
+    confirmation_url = url_for('activate_user', user_id=user['_id'], _external=True)
+    mail_to_be_sent.body = "Dear %s, click here to confirm: %s" % (user['email'], confirmation_url)
+    from app import mail
+    mail.send(mail_to_be_sent)
+
+
 
 @app.route('/registration')
 def registration():
