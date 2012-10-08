acdbda0d56371fa3e0ae9512adabacdb15c74acf
Alexander Davydov <nyddle@gmail.com>
2012-10-08 15:16:29 -0400

    some moudules added

--- app.py
+++ app.py
@@ -5,13 +5,49 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 from flask.ext.mail import Mail, Message
 from werkzeug import check_password_hash, generate_password_hash
 
+from mongokit import Connection, Document
+
+
 SECRET_KEY = 'development key'
+
 
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 mail = Mail(app)
 
+# connect to the database
+connection = Connection('mongodb://quickhunt:qhpassword@alex.mongohq.com:10013/app8222672')
+def max_length(length):
+    def validate(value):
+        if len(value) <= length:
+            return True
+        raise Exception('%s must be at most %s characters long' % length)
+    return validate
+
+@connection.register
+class User(Document):
+    structure = {
+        'password': unicode,
+        'email': unicode,
+    }
+    validators = {
+        'password': max_length(120),
+        'email': max_length(120)
+    }
+    required_fields = ['password', 'email']
+    use_dot_notation = True
+    def __repr__(self):
+        return '<User %r>' % (self.email)
+
+    #database:app8222672
+
+
+
+user =  connection.app8222672.users.User()
+user.passwors = u'password'
+user.email = u'email@host.com'
+user.save()
 
 @app.route('/')
 def hello():
