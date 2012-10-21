a73a58c232be716c3906b5eca6563a13a0390036
nyddle <nyddle@gmail.com>
2012-10-22 00:44:08 +0400

    json

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 import os
+import json
 from flask import Flask, render_template
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-             render_template, flash
-from flask.ext.mail import Mail, Message
+             render_template, flash, jsonify, Response
 import pymongo
 from pymongo import Connection
 
@@ -14,7 +15,6 @@ SECRET_KEY = 'development key'
 app = Flask(__name__)
 app.debug = True
 app.config.from_object(__name__)
-mail = Mail(app)
 if app.debug:
     from flaskext.lesscss import lesscss
     lesscss(app)
@@ -23,8 +23,8 @@ app.static_path = 'static'
 
 # connect to the database
 connection = Connection('mongodb://quickhunt:qhpassword@alex.mongohq.com:10013/app8222672')
-users_collection =  connection.app8222672.users
-print users_collection
+jobs_collection =  connection.app8222672.jobs
+print jobs_collection
 
 
 @app.route('/')
@@ -32,16 +32,54 @@ def hello():
     return render_template('add.html')
 
 
-
-@app.route('/add')
+@app.route('/edit')
 def registration():
     return render_template('add.html')
 
 
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+            'status': 404,
+            'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+@app.route('/jobs', methods = ['GET'])
+def get_job():
+    users = {'1':'john', '2':'steve', '3':'bill'}
+    
+    if userid in users:
+        return jsonify({userid:users[userid]})
+    else:
+        return not_found()
+	return undef
+
+@app.route('/jobs', methods = ['POST'])
+def create_job():
+	#print "POST:" + request.data
+	js = json.dumps(request.data)
+	json_data = json.loads(request.data)
+	print json_data
+	resp = Response(js, status=200, mimetype='application/json')
+	return resp
+
+@app.route('/jobs', methods = ['DELETE'])
+def delete_job():
+	return undef
+
+
+
+
+
+"""
 @app.route('/<anypage>')
 def anypage(anypage):
     return render_template(anypage+'.html')
-
+"""
 
 
 if __name__ == '__main__':
@@ -49,4 +87,23 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
+"""
+  <form name="add_job" action="">
+      <label>Вакансия: <input type="text" name="job_title"></label>
+      <label>Категория: <input type="password" name="job_category"></label>
+        <span><input type="radio" name="job_employment" value="full_time" checked="checked"> полная</span>
+        <span><input type="radio" name="job_employment" value="part_time"> частичная</span>
+        <span><input type="radio" name="job_workplace" value="office" checked="checked"> офис</span>
+        <span><input type="radio" name="job_workplace" value="remote"> удаленно</span>
+        <textarea name="job_description"></textarea>
+        <textarea name="job_charge"></textarea>
+        <textarea name="job_experience"></textarea>
+        <textarea name="job_additional"></textarea>
+        <textarea name="job_terms"></textarea>
+
+      <label>Компания: <input type="text" name="company_name"></label>
+        <textarea name="company_description"></textarea>
+      <label>Лого: <input type="file" name="company_logo"></label>
+
+"""
 
--- static/js/init.js
+++ static/js/init.js
@@ -1,3 +1,44 @@
 $(document).ready(function(){
-	alert('Hello world!');
+
+  $.fn.serializeJSON = function(){
+    var json = {}
+    var form = $(this);
+    form.find('input, select').each(function(){
+      var val
+      if (!this.name) return;
+
+      if ('radio' === this.type) {
+        if (json[this.name]) { return; }
+
+        json[this.name] = this.checked ? this.value : '';
+      } else if ('checkbox' === this.type) {
+        val = json[this.name];
+
+        if (!this.checked) {
+          if (!val) { json[this.name] = ''; }
+        } else {
+          json[this.name] =
+            typeof val === 'string' ? [val, this.value] :
+            $.isArray(val) ? $.merge(val, [this.value]) :
+            this.value;
+        }
+      } else {
+        json[this.name] = this.value;
+      }
+    })
+    return json;
+  }
+
+	$('#addjobbutton').click(function() {
+		//alert(JSON.stringify($('#addjobform').serializeJSON()));
+		$.ajax({
+		  contentType: "application/json",
+		  type: "POST",
+		  url: "/jobs",
+		  data: JSON.stringify($('#addjobform').serializeJSON())
+		}).done(function( msg ) {
+		  alert( "Server returned: " + msg );
+		});
+	});
+
 });
