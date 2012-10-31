22f3a2efe8f9262c858938040cba2845011cb08c
Alexander Davydov <nyddle@gmail.com>
2012-10-31 06:48:22 -0400

    sdv

--- manage.py
+++ manage.py
@@ -2,20 +2,22 @@
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from flask.ext.script import Manager, Server
-from quickhunt import app
+from flask.ext.script import Manager, Server, Command, Option
+from quickhunt import create_app
 
-manager = Manager(app)
+manager = Manager(create_app)
+
+manager.add_option('-e', '--env', dest='env', required=False, default='debug')
 
 # Turn on debugger by default and reloader
 manager.add_command("runserver", Server(
-    use_debugger = True,
     use_reloader = True,
     host = '0.0.0.0')
 )
 
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    host='0.0.0.0'
     manager.run()
 
