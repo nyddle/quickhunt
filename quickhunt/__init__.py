80cdcc50202354b1656555e848be0f75f817c778
Alexander Davydov <nyddle@gmail.com>
2012-11-30 16:05:32 +0400

    settings

--- quickhunt/__init__.py
+++ quickhunt/__init__.py
@@ -14,6 +14,8 @@ import urllib2
 from redis_completion import RedisEngine
 import urlparse
 import redis
+import requests
+from requests.auth import HTTPBasicAuth
 
 
 def create_app(env='debug'):
@@ -44,6 +46,7 @@ def create_app(env='debug'):
     # connect to the database
     connection = Connection('mongodb://quickhunt:qhpassword@alex.mongohq.com:10013/app8222672')
     jobs_collection =  connection.app8222672.jobs
+    users_collection =  connection.app8222672.users
     url = urlparse.urlparse('redis://:bSpyvbncbJCCMn6sjb@pikachu.ec2.myredis.com:6777')
     autocomplete_engine = RedisEngine(host=url.hostname, port=url.port, password=url.password)
 
@@ -87,6 +90,60 @@ def create_app(env='debug'):
     def edit(jobid):
         return render_template('add.html', job=jobid)
 
+    @app.route('/registration')
+    def registration():
+        return render_template('registration.html')
+
+    def get_user_id(email):
+        return users_collection.find_one({ 'email' : email })
+
+    @app.route('/login')
+    def registration():
+        return render_template('login.html')
+
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        """Registers the user."""
+        g.user = None
+        if g.user:
+            return render_template('registration.html')
+        error = None
+        if request.method == 'POST':
+            if not request.form['email'] or \
+                     '@' not in request.form['email']:
+                error = 'You have to enter a valid email address'
+            elif not request.form['password']:
+                error = 'You have to enter a password'
+            elif get_user_id(request.form['email']) is not None:
+                error = 'The username is already taken'
+            else:
+                new_user_id = users_collection.save({ 'email' : request.form['email'], 'password' : request.form['password'], 'status' : 'awaiting confirm' })
+                payload = {'from': 'Excited User <me@samples.mailgun.org>', 'to': request.form['email'], 'subject': 'Quick Hunt account confirmation', 'text': 'http://obscure-springs-3022.herokuapp.com/activate_user/' + str(new_user_id) }
+                r = requests.post("https://api.mailgun.net/v2/app8222672.mailgun.org/messages", auth=HTTPBasicAuth('api', 'key-9m9vuzkafbyjqhm9ieq71n0lu9dgf9b9'), data=payload)
+                flash('You were successfully registered. Confirm registration and login.')
+                return render_template('login.html', error=error) 
+        flash('no luck ((' + request.method)
+        return render_template('registration.html', error=error)
+
+
+
+    @app.route('/activate_user/<user_id>')
+    def activate_user(user_id):
+        """
+        Activate user function.
+        """
+        found_user = users_collection.find_one({'_id':bson.ObjectId(oid=str(user_id))});
+        if not found_user:
+            return abort(404)
+        else:
+            if found_user['status'] == 'awaiting_confirm':
+                ### Setting the user status active here ###*
+                payload = {'from': 'Quick Hunt <me@samples.mailgun.org>', 'to': found_user['email'], 'subject': 'Quick Hunt account confirmation', 'text': 'Subscription confirmed.' }
+                r = requests.post("https://api.mailgun.net/v2/app8222672.mailgun.org/messages", auth=HTTPBasicAuth('api', 'key-9m9vuzkafbyjqhm9ieq71n0lu9dgf9b9'), data=payload)
+                flash('user has been activated', 'info')
+            elif found_user['status'] == 'active':
+                flash('user already activated', 'info')
+            return redirect(url_for('content'))
 
 
     """ This is the API part of the equation """
@@ -142,7 +199,6 @@ def create_app(env='debug'):
         jobs_collection.save(json_data)
         resp = Response(js, status=200, mimetype='application/json')
         return resp
-
 
     @app.route('/api/jobs/<jobid>', methods = ['DELETE'])
     def delete_job(jobid):
